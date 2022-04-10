c2a6b227ec8ddc15cfc6117b13688db569c284ab
Tanvir Aumi <tanvir@cs.washington.edu>
2022-04-09 23:03:18 -0700

    Added Spacy model into dockerfile

--- py/downloadModels.sh
+++ py/downloadModels.sh
@@ -10,3 +10,7 @@ DATA_DIR=$1
 if [ ! -f $DATA_DIR/lid.176.bin ]; then
     wget -P $DATA_DIR -nv --show-progress --progress=bar:force:noscroll https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin
 fi
+
+if [ ! -f $DATA_DIR/en_core_web_lg-3.0.0-py3-none-any.whl ]; then
+    wget -P $DATA_DIR -nv --show-progress --progress=bar:force:noscroll https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-3.0.0/en_core_web_lg-3.0.0-py3-none-any.whl
+fi
